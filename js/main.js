1707fd029820779f1fc1f16dca382dc734baf93f
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2023-04-25 22:05:45 -0300

    CRUD finalizado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
-const itens = [];
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
+
+itens.forEach(elemento => {
+    criaElemento(elemento);
+});
 
 form.addEventListener('submit', (evento) => {
 
@@ -8,33 +12,76 @@ form.addEventListener('submit', (evento) => {
 
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+    const itemAtual = {
+        'nome': nome.value,
+        'quantidade': quantidade.value
+    }
 
-    criaElemento(nome.value, quantidade.value);
+    if (existe) {
+
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+
+    } else {
+
+        itemAtual.id = itens[itens.length -1]? (itens[itens.length -1]).id +1 : 0;
+        criaElemento(itemAtual);
+        itens.push(itemAtual);
+
+    }
+
+    localStorage.setItem('itens', JSON.stringify(itens));
 
     nome.value = '';
     quantidade.value = '';
 
 });
 
-function criaElemento (nome, quantidade) {
+function criaElemento (item) {
     
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
+    numeroItem.dataset.id = item.id;
+    numeroItem.innerHTML = item.quantidade;
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += item.nome;
+
+    novoItem.appendChild(botaoDeleta(item.id));
 
     lista.appendChild(novoItem);
 
-    const itemAtual = {
-        'nome': nome,
-        'quantidade': quantidade
-    }
+}
 
-    itens.push(itemAtual);
+function atualizaElemento (item) {
 
-    localStorage.setItem('item', JSON.stringify(itens));
+   document.querySelector('[data-id="' + item.id + '"]').innerHTML = item.quantidade;
+
+}
+
+function botaoDeleta (id) {
+
+    const elementoBotao = document.createElement('button');
+    elementoBotao.innerText = "X";
+    elementoBotao.addEventListener('click', function () {
+        deletaElemento(this.parentNode, id);
+    });
+    
+    return elementoBotao
+
+}
+
+function deletaElemento (tag, id) {
+
+    tag.remove();
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem('itens', JSON.stringify(itens));
+
+
 }
